85a43c7d508763ad03b1b71f4c66daea34ea91c6
Davis Shoup <davisshoup@yahoo.com>
2022-05-10 17:21:04 -0400

    Added the armor function into JS.

--- script.js
+++ script.js
@@ -1,41 +1,48 @@
-// const weaponURL = "https://eldenring.fanapis.com/api/weapons";
-
-// function createWeapon (weapon) {
-//     const $description = $("<td>", { class: "description" });
-//     const $title = $("<td>", { class: "title" });
-//     const $imageTag = $("<td>", { class: "imageTag" });
-//     $imageTag.append("<img src=" + weapon.image + ">");
-//     $title.text(weapon.name);
-//     $description.text(weapon.description);
-//     return $('<table/>').append($title).append($imageTag).append($description);
-// }
-// $.ajax(weaponURL).then(function(response){
-//     const weaponData = response.data;
-//     const $weapons = weaponData.map(weapon => createWeapon(weapon));
-//     $('.main').append($weapons);
-// }, function(error) {
-//     console.log('error');
-//     console.log(error);
-// });
-
-
 const weaponURL = "https://eldenring.fanapis.com/api/weapons";
 
-function createWeapon (weapon) {
+function createWeapon(weapon) {
     const $description = $("<p>", { class: "description" });
     const $title = $("<h2>", { class: "title" });
     const $image = $("<img>", { class: "weaponimage" });
     const $category = $("<p>", { class: "category" });
-    $category.text("Weapon Type: " + weapon.category);
+    const $weight = $("<p>", { class: "weight" });
+    $weight.text("Weight - " + weapon.weight);
+    $category.text("Weapon Type - " + weapon.category);
     $title.text(weapon.name);
-    $description.text("Description: " +weapon.description);
+    $description.text("Description - " + weapon.description);
     $image.attr("src", weapon.image);
-    return $('<div/>').append($title).append($image).append($category).append($description);
-}
+    return $('<div/>').append($title).append($image).append($category).append($weight).append($description);
+};
+
 $.ajax(weaponURL).then(function(response){
     const weaponData = response.data;
     const $weapons = weaponData.map(weapon => createWeapon(weapon));
     $('.main').append($weapons);
+}, function(error) {
+    console.log('error');
+    console.log(error);
+});
+
+const armorURL = "https://eldenring.fanapis.com/api/armors";
+
+function createArmor(armor){
+    const $description = $("<p>", { class: "description" });
+    const $title = $("<h2>", { class: "title" });
+    const $image = $("<img>", { class: "armorimage" });
+    const $category = $("<p>", { class: "category" });
+    const $weight = $("<p>", { class: "weight" });
+    $weight.text("Weight - " + armor.weight);
+    $category.text("Armor Type - " + armor.category);
+    $title.text(armor.name);
+    $description.text("Description - " + armor.description);
+    $image.attr("src", armor.image);
+    return $('<div/>').append($title).append($image).append($category).append($weight).append($description);
+};
+$.ajax(armorURL).then(function(response){
+    const armorData = response.data;
+    const $armors = armorData.map(armor => createArmor(armor));
+    $('.check').append($armors);
+    return;
 }, function(error) {
     console.log('error');
     console.log(error);
