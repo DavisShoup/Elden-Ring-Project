507b958826ffcc16dc6419c55c57b9255f1c2f48
Davis Shoup <davisshoup@yahoo.com>
2022-05-10 12:44:43 -0400

    Changed background video to a fixed position so during scrolling it moves with the user, added a weapon CSS file to create a new grid on how I want the weapons to display. I was also debating on if I should be creating a table with all the elements i'm creating from my API, which is why I have it commed out. Ultimately, I think I will delete the code for the table because the elements didn't line up well even with some CSS.

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
+// const weaponURL = "https://eldenring.fanapis.com/api/weapons";
+
+// function createWeapon (weapon) {
+//     const $description = $("<td>", { class: "description" });
+//     const $title = $("<td>", { class: "title" });
+//     const $imageTag = $("<td>", { class: "imageTag" });
+//     $imageTag.append("<img src=" + weapon.image + ">");
+//     $title.text(weapon.name);
+//     $description.text(weapon.description);
+//     return $('<table/>').append($title).append($imageTag).append($description);
+// }
+// $.ajax(weaponURL).then(function(response){
+//     const weaponData = response.data;
+//     const $weapons = weaponData.map(weapon => createWeapon(weapon));
+//     $('.main').append($weapons);
+// }, function(error) {
+//     console.log('error');
+//     console.log(error);
+// });
+
+
 const weaponURL = "https://eldenring.fanapis.com/api/weapons";
 
 function createWeapon (weapon) {
     const $description = $("<p>", { class: "description" });
     const $title = $("<h2>", { class: "title" });
-    const $image = $("<img>", { class: "image" });
+    const $image = $("<img>", { class: "weaponimage" });
+    const $category = $("<p>", { class: "category" });
+    $category.text("Weapon Type: " + weapon.category);
     $title.text(weapon.name);
-    $description.text(weapon.description);
+    $description.text("Description: " +weapon.description);
     $image.attr("src", weapon.image);
-    return $('<div/>').append($title).append($image).append($description);
+    return $('<div/>').append($title).append($image).append($category).append($description);
 }
 $.ajax(weaponURL).then(function(response){
     const weaponData = response.data;
