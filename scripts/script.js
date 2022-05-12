9bc02ee04e190c7f7dcbc741f72688618eacfef1
Davis Shoup <davisshoup@yahoo.com>
2022-05-12 15:55:43 -0400

    Made a few updates, tried to make a search function

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,25 @@
 // WEAPON FUNCTION (BELOW)
 
-const weaponURL = "https://eldenring.fanapis.com/api/weapons?limit=100"; // declaring 
+const weaponURL = "https://eldenring.fanapis.com/api/weapons?limit=100"; // declaring url variable
 
-function createWeapon(weapon) {
-    const $description = $("<p>", { class: "description" });
+function createWeapon(weapon) { // the function we are using to map over the array of data and create elements for a new array
+    const $description = $("<p>", { class: "description" }); // declaring the description of the weapon as a variable, giving it an HTML tag and classifying it
     const $title = $("<h2>", { class: "title" });
-    const $image = $("<img>", { class: "image" });
-    const $category = $("<p>", { class: "category" });
-    const $weight = $("<p>", { class: "weight" });
-    $weight.text("Weight - " + weapon.weight);
+    const $image = $("<img>", { class: "image" }); 
+    const $category = $("<p>", { class: "category" }); 
+    const $weight = $("<p>", { class: "weight" }); 
+    $weight.text("Weight - " + weapon.weight); // inside each newly made HTML tag, we are adding the text from the data
     $category.text("Weapon Type - " + weapon.category);
     $title.text(weapon.name);
     $description.text("Description - " + weapon.description);
-    $image.attr("src", weapon.image);
-    return $('<div/>').append($title).append($image).append($category).append($weight).append($description);
+    $image.attr("src", weapon.image); // adding a src attribute and attaching the image from the data to the newly made img tags
+    return $('<div/>').append($title).append($image).append($category).append($weight).append($description); //returning the new array
 };
 
-$.ajax(weaponURL).then(function(response){
-    const weaponData = response.data;
-    const $weapons = weaponData.map(weapon => createWeapon(weapon));
-    $('.main').append($weapons); 
+$.ajax(weaponURL).then(function(response){ //using ajax to invoke a function to pull data from the URL
+    const weaponData = response.data; // storing the data from the URL to a variable
+    const $weapons = weaponData.map(weapon => createWeapon(weapon)); //assigning the mapping of the data to a variable + we are passing a function to create a new array of elements (weapon name/image/category/weight/etc) from the API
+    $('.mainweapon').append($weapons); // append the new array to the .mainweapon section of the HTML
 }, function(error) {
     console.log('error');
     console.log(error);
@@ -46,7 +46,7 @@ function createArmor(armor){
 $.ajax(armorURL).then(function(response){
     const armorData = response.data;
     const $armors = armorData.map(armor => createArmor(armor));
-    $('.check').append($armors);
+    $('.mainarmors').append($armors);
 }, function(error) {
     console.log('error');
     console.log(error);
@@ -111,3 +111,35 @@ $.ajax(itemsURL).then(function(response){
 });
 
 // MISC FUNCTION (ABOVE)
+
+
+// const $name = $(".title");
+// const $type = $("main");
+// const $form = $("form");
+// const $input = $('input[type="text"]');
+
+// function searchWeapon(weapon) { 
+//     $name.text(weapon.name);
+//     return $name
+// };
+
+// $form.on('submit', handleGetData)
+// function handleGetData(event){
+//     event.preventDefault();
+//     const userInput = $input.val();
+//     const promise = $.ajax({
+//         url: `https://eldenring.fanapis.com/api/weapons?name=${userInput}`
+//     });
+//     promise.then((response) => {
+//         const $accessData = response.data;
+//         const $weaponData = $accessData.map(weapon => searchWeapon(weapon));
+//         const renderData = () => {
+//             $name.text($weaponData.name);
+//         }
+//         renderData();
+//     }, function(error) {
+//         console.log('error');
+//         console.log(error);
+//     });
+// }
+
